e3d331dcf239d1548510a59bb8748e8d3258f23e
novikovleha200316-prog <novikov.leha200316@gmail.com>
2026-05-25 22:34:09 +0300

    Delete rpgsaga/lint-staged.config.mjs

--- rpgsaga/lint-staged.config.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('lint-staged').Configuration} */
-export default {
-  "*.{ts,tsx,js,mjs,cjs}": ["eslint --fix --max-warnings=0", "prettier --write"],
-  "*.{json,md,yml,yaml}": "prettier --write",
-};
